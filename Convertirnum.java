31e7eb8ae0c73f7a593179d82d3f08564f6afc77
SebastianVerdugo <113747168+SebastianVerdugo@users.noreply.github.com>
2022-09-25 22:39:35 -0600

    Update Convertirnum.java

--- Convertirnum.java
+++ Convertirnum.java
@@ -20,3 +20,15 @@ public class Convertirnum {
 	}
 
 }
+	/*
+		return convertidor(residuo=valor%10,valor=valor/10,  r=residuo+r);
+		
+		convertidor(4, 2340, "4")
+		convertidor(0, 234, "04")
+		convertidor(4, 23, "404")
+		convertidor(3, 2, "3404")
+		
+		Se cumple la condicion valor<10
+		
+		retorna 23404
+	*/
